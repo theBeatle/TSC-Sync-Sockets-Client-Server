90ec260f2f5145d4546cf771661e92e7e0674ba4
DESKTOP-EPN9CP6\thebe <oleh.knyaz@gmail.com>
2019-03-26 11:50:37 +0200

    Network Sync Sockets

---
+++ Client/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] buffer = new byte[1024];
+
+            try
+            {
+                // to get owner IP
+                //IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName()); //Server IP
+                //IPAddress ip = ipHostInfo.AddressList[3]; // moge ne 0  //Server IP
+                //
+                //foreach (var item in ipHostInfo.AddressList)
+                //{
+                //    Console.WriteLine($"Ip addr - {item}");
+                //}
+                //connection 
+                var serverIP = new IPAddress(new byte[] { 10, 7, 180, 104 });
+                //IPEndPoint remotePort = new IPEndPoint(ip, 11000);
+                IPEndPoint remotePort = new IPEndPoint(serverIP, 11000);
+
+                //socket creation
+                // IPv4, Stream, TCP
+                Socket socket = new Socket(serverIP.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    Console.WriteLine("Press any key to connect!");
+                    Console.ReadKey();
+                    socket.Connect(remotePort);
+                    Console.WriteLine($"Connected to {socket.RemoteEndPoint}");
+                
+
+                    byte[] msg = Encoding.ASCII.GetBytes("something EOT");
+
+                    //sent to server
+                    int sendBytes = socket.Send(msg);
+
+                    //receive answer from server
+                    int receivedBytes = socket.Receive(buffer);
+                    Console.WriteLine($"Received: {Encoding.ASCII.GetString(buffer,0,receivedBytes)}");
+
+                    //free socket
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
--- ConsoleApp50/Program.cs
+++ ConsoleApp50/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,67 @@ namespace ConsoleApp50
 {
     class Program
     {
+        public static string data = null;
         static void Main(string[] args)
         {
+            byte[] buffer = new byte[1024];
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ip = ipHostInfo.AddressList[3]; // moge ne 0
+
+            foreach (var item in ipHostInfo.AddressList)
+            {
+                Console.WriteLine($"Ip addr - {item}");
+            }
+            //connection 
+            IPEndPoint localPort = new IPEndPoint(ip, 11000);
+
+            //socket creation
+            // IPv4, Stream, TCP
+            Socket socket = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                socket.Bind(localPort);
+                socket.Listen(100);
+
+                //loop for connection acceptance
+                while (true)
+                {
+                    Console.WriteLine("Waitting for connection...");
+                    Socket handler = socket.Accept();
+                    data = null;
+
+                    //loop for data receiving
+                    while (true)
+                    {
+                        buffer = new byte[1024];
+                        int receivedBytes = handler.Receive(buffer);
+                        data += Encoding.ASCII.GetString(buffer, 0, receivedBytes);
+                        if (data.IndexOf("EOT") > -1)
+                        {
+                            break;
+                        }
+
+                    }
+                    Console.WriteLine($"Received: {data.Length} bytes." +
+                        $" \nUseless data is :{data}");
+
+                    //echo effect
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    handler.Send(msg);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
         }
     }
 }
